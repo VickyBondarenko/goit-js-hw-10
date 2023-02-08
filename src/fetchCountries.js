f9d898285d4415624c786e7e31f847ea0ddf60c9
VickyBondarenko <quikyv@gmail.com>
2023-02-08 20:18:12 +0100

    change if

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -6,7 +6,7 @@ function fetchCountries(name) {
   return fetch(
     ` https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
   ).then(response => {
-    if (!response.ok) {
+    if (!response.ok || response.status === 404) {
       throw Error('Oops, there is no country with that name');
     }
     return response.json();
